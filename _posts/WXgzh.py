d48e6e6df856a2bee49cc4acae4ccee304b4155b
kcdyx1 <kcdyx1@hotmail.com>
2018-09-01 23:29:20 +0800

    更新

--- _posts/WXgzh.py
+++ _posts/WXgzh.py
@@ -1,14 +1,35 @@
 import wechatsogou
+import datetime
 
-gzh_list = ['后沙']
+
+nowTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+gzh_list = ['后沙', '全球技术地图', '人民日报', '战略前沿技术', 'KPMG大数据挖掘', 'AI科技评论']
 ws_api = wechatsogou.WechatSogouAPI(captcha_break_time=3)
-def gzh_xx(gzh_name):
+gzh_dict = {}
+
+with open('weixintemp.txt', 'r', encoding="utf-8") as f:
+    lines = f.readlines()
+
+with open("2018-08-21-微信公众号.md", "w", encoding="utf-8") as f_w:
+    for line in lines:
+        f_w.write(line)
+
+
+def get_gzh_xx(gzh_name):
     # 调用API获取公众号信息
     try:
         gzh_info = ws_api.get_gzh_info(gzh_name)
-        print("获取成功！---->")
-        for k, v in gzh_info.items():
-            print(k, ": ", v, '\n')
-        return 
+        gzh_dict[gzh_name] = gzh_info
+        return gzh_dict
     except:
-        print("获取失败")
+        print(gzh_name, "获取失败")
+
+
+for n in gzh_list:
+    results = get_gzh_xx(n)
+
+with open("2018-08-21-微信公众号.md", "a", encoding="utf-8") as f_w:
+    for k, v in results.items():
+        tiaomu = (
+            '- [' + k + '](' + v['profile_url'] + '){:target="_blank"}\n')
+        f_w.write(tiaomu)
